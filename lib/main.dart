7abbfa2b80e25dbf9e7984f95b4c0bace183c149
Asif2011 <asfrza2008@gmail.com>
2021-07-18 18:38:37 +0500

    from Theme font family controlling, Elevated button and its style, padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_2_2_test/screens/homeScreen.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/loginScreen.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      themeMode:ThemeMode.dark ,
+      themeMode:ThemeMode.dark,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
@@ -25,7 +26,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        // primarySwatch: Colors.green,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        colorScheme: ColorScheme.light(),
 
       ),
       // home: HomeScreen(),
--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,5 +1,4 @@
 import  'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -7,22 +6,52 @@ class LoginScreen extends StatelessWidget {
     return Container(
       child: Material(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-            Image.asset('assets/images/login.png'),
-              Center(
-              child:Text("login screen",
-              style:GoogleFonts.arimo(),
-              // style: TextStyle(
-              //   fontStyle: FontStyle.italic,
-              //   color: Colors.pink,
-              //   decorationStyle: TextDecorationStyle.wavy,
-              //   fontWeight: FontWeight.bold,
-              //   fontSize: 20,
-              //   ),
-              )
+          child: Padding(padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/login.png"),
+                SizedBox(height: 20,),
+                Text("Welcome",
+                // style:GoogleFonts.arimo(),
+                ),
+                TextField(
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    hintText:"Enter Username",
+                    labelText: "Username",
+                    ),
+                ),
+                TextField(
+                  obscureText: true,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    hintText:"Enter Password",
+                    labelText: "Password",
+                    ),
+                ),
+                SizedBox(height: 20,),
+                ElevatedButton(
+                  onPressed: (){},
+                  style: ButtonStyle(
+                    // foregroundColor: MaterialStateProperty.all(Colors.yellowAccent),
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed))
+                      {
+                        return Colors.white10.withOpacity(0.5);
+                      }
+                      else{
+                        return Colors.purple.withOpacity(0.5);
+                      }
+                      
+                    }, // Use the component's default.
+                  ),                  
+                ),
+                child: Text("Login"),
+                ),
+              ]
             ),
-            ]
           ),
         ),
       )
