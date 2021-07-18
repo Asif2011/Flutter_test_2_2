e8d26d16240b6cc7cb3d57bab443de9fcf28bc18
Asif2011 <asfrza2008@gmail.com>
2021-07-19 01:49:45 +0500

    use of navigator

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,60 +1,64 @@
-import  'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Material(
-        child: SingleChildScrollView(
-          child: Padding(padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset("assets/images/login.png"),
-                SizedBox(height: 20,),
-                Text("Welcome",
-                // style:GoogleFonts.arimo(),
-                ),
-                TextField(
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                    hintText:"Enter Username",
-                    labelText: "Username",
-                    ),
-                ),
-                TextField(
-                  obscureText: true,
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                    hintText:"Enter Password",
-                    labelText: "Password",
-                    ),
-                ),
-                SizedBox(height: 20,),
-                ElevatedButton(
-                  onPressed: (){},
-                  style: ButtonStyle(
-                    // foregroundColor: MaterialStateProperty.all(Colors.yellowAccent),
-                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                    (Set<MaterialState> states) {
-                      if (states.contains(MaterialState.pressed))
-                      {
-                        return Colors.white10.withOpacity(0.5);
-                      }
-                      else{
-                        return Colors.purple.withOpacity(0.5);
-                      }
-                      
-                    }, // Use the component's default.
-                  ),                  
-                ),
-                child: Text("Login"),
-                ),
-              ]
+        child: Material(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.scaleDown,
             ),
-          ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 22) ,
+              // style:GoogleFonts.arimo(),
+            ),
+            TextField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText: "Enter Username",
+                labelText: "Username",
+              ),
+            ),
+            TextField(
+              obscureText: true,
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText: "Enter Password",
+                labelText: "Password",
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {Navigator.pushNamed(context, "/home");},
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all(Size(80,40)),
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.white10.withOpacity(0.5);
+                    } else {
+                      return Colors.purple.withOpacity(0.5);
+                    }
+                  }, // Use the component's default.
+                ),
+              ),
+              child: Text("Login"),
+            ),
+          ]),
         ),
-      )
-    );
+      ),
+    ));
   }
 }
